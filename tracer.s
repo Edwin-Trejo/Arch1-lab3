47bdae9e690da6af8d4ef6f18ae8a8d0160892b9
Edwin-Trejo <60363120+Edwin-Trejo@users.noreply.github.com>
2022-04-06 13:59:34 -0600

    Fixed comments

--- tracer.s
+++ tracer.s
@@ -1,7 +1,7 @@
-;I received help from Christian Gomez while working on tracer.
+#I received help from Christian Gomez while working on tracer.
 
 .global tracer  
 
 tracer:         
-        mv a0,sp        ;copies the value from register a0, to register sp which is the stack pointer.
-        jalr x0,0(x1)   ;jump and link register
+        mv a0,sp        #copies the value from register a0, to register sp which is the stack pointer.
+        jalr x0,0(x1)   #jump and link register
